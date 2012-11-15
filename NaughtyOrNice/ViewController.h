36c38c7cf273a62d07a37620a0421a8549eff5f1
T. Andrew Binkowski <abinkowski@uchicago.edu>
2012-11-14 23:09:59 -0600

    Initial Commit

---
+++ NaughtyOrNice/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NaughtyOrNice
+//
+//  Created by T. Andrew Binkowski on 11/14/12.
+//  Copyright (c) 2012 T. Andrew Binkowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ NaughtyOrNice/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NaughtyOrNice
+//
+//  Created by T. Andrew Binkowski on 11/14/12.
+//  Copyright (c) 2012 T. Andrew Binkowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
